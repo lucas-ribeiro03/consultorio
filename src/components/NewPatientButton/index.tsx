79d09775ad5ce95b5b099891b3fdbb33e7c73ae6
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-10-15 09:47:23 -0300

    aria label and title

--- src/components/NewPatientButton/index.tsx
+++ src/components/NewPatientButton/index.tsx
@@ -2,7 +2,11 @@ import { PlusIcon } from "lucide-react";
 
 export const NewPatientButton = () => {
   return (
-    <button className="absolute right-10 bottom-10 bg-blue-500 text-amber-50 p-5 rounded-full cursor-pointer hover:bg-blue-700 transition duration-150">
+    <button
+      aria-label="Adicionar paciente"
+      title="Adicionar paciente"
+      className="absolute right-10 bottom-10 bg-blue-500 text-amber-50 p-5 rounded-full cursor-pointer hover:bg-blue-700 transition duration-150"
+    >
       <PlusIcon />
     </button>
   );
