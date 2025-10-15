7f58ec386edf4af9445110991e3bc33c8de25d51
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-10-15 10:13:31 -0300

    patient schema

---
+++ src/mysql/schemas/pacientes.ts
@@ -0,0 +1,13 @@
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const PatientTable = sqliteTable("pacientes", {
+  id: text("id").primaryKey(),
+  nome: text("nome").notNull(),
+  matricula: text("matricula").notNull().unique(),
+  idade: integer("idade").notNull(),
+  dataNasc: text("dataNasc").notNull(),
+  tel1: text("tel1").notNull(),
+  tel2: text("tel1"),
+  convenio: text("convenio"),
+  cpf: text("cpf").notNull().unique(),
+});
