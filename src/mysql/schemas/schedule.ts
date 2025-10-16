83cb1e107d7ad062af4877d160918a810dade588
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-10-16 15:13:26 -0300

    id key

--- src/mysql/schemas/schedule.ts
+++ src/mysql/schemas/schedule.ts
@@ -1,7 +1,7 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const AgendaTable = sqliteTable("agenda", {
-  id: integer("id").primaryKey(),
+  id: text("id").primaryKey(),
   dia: integer("dia").notNull(),
   mes: integer("mes").notNull(),
   ano: integer("mes").notNull(),
