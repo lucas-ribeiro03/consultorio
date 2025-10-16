0c395f208e8ae294532d968645f83b575abfce9f
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-10-16 15:54:34 -0300

    parameters

--- src/repositories/pacientes/patient-repository.ts
+++ src/repositories/pacientes/patient-repository.ts
@@ -1,12 +1,12 @@
 import { PatientModel } from "@/models/pacientes-model";
 
 export interface PatientRepository {
-  findAll(): Promise<PatientModel>;
-  findByName(): Promise<PatientModel>;
-  findByMatricula(): Promise<PatientModel>;
-  findByCpf(): Promise<PatientModel>;
+  findAll(): Promise<PatientModel[]>;
+  findByName(name: string): Promise<PatientModel | PatientModel[]>;
+  findByMatricula(matricula: number): Promise<PatientModel>;
+  findByCpf(cpf: string): Promise<PatientModel>;
 
   //Mutation
   createPatient(patient: PatientModel): Promise<PatientModel>;
-  deletePatient(matricula: string): Promise<PatientModel>;
+  deletePatient(matricula: number): Promise<PatientModel>;
 }
