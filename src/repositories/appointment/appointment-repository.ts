40a52b6feecbadea542e059abe94db74bc354769
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-10-16 15:55:52 -0300

    parameter type

--- src/repositories/appointment/appointment-repository.ts
+++ src/repositories/appointment/appointment-repository.ts
@@ -3,7 +3,7 @@ import { AppointmentModel } from "@/models/appointment-model";
 export interface AppointmentRepository {
   findAll(): Promise<AppointmentModel[]>;
   findByPatientName(name: string): Promise<AppointmentModel>;
-  findByPatientMatricula(matricula: string): Promise<AppointmentModel>;
+  findByPatientMatricula(matricula: number): Promise<AppointmentModel>;
   // findByDate(date: string): Promise<AppointmentModel>;
 
   createSchedule(appointment: AppointmentModel): Promise<AppointmentModel>;
