b348acd2ad59508bfe27b3323127c02fbfd52985
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-10-16 10:13:41 -0300

    new patient form

---
+++ src/app/patient/new_patient/page.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@/components/Container";
+import { NewPatientForm } from "@/components/NewPatientForm";
+
+export default function NewPatient() {
+  return (
+    <Container>
+      <NewPatientForm />
+    </Container>
+  );
+}
