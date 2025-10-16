f4bc48c2673b60e6ce036c70e2d9265438f8000c
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2025-10-16 14:26:54 -0300

    new schema names

--- src/mysql/index.ts
+++ src/mysql/index.ts
@@ -1,9 +1,9 @@
 import { resolve } from "path";
 import Database from "better-sqlite3";
 import { drizzle } from "drizzle-orm/better-sqlite3";
-import { PatientTable } from "./schemas/pacientes";
-import { ScheduleTable } from "./schemas/schedule";
-import { AgendaTable } from "./schemas/agenda";
+import { PatientTable } from "./schemas/patient";
+import { AppointmentTable } from "./schemas/appointment";
+import { AgendaTable } from "./schemas/schedule";
 
 const sqliteDatabasePath = resolve(process.cwd(), "db.sqlite3");
 const sqliteDatabase = new Database(sqliteDatabasePath);
@@ -11,8 +11,8 @@ const sqliteDatabase = new Database(sqliteDatabasePath);
 export const drizzleDb = drizzle(sqliteDatabase, {
   schema: {
     patient: PatientTable,
-    schedule: ScheduleTable,
-    agenda: AgendaTable,
+    appointment: AppointmentTable,
+    schedule: AgendaTable,
   },
   logger: false,
 });
